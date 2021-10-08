9a246549f20cb276a965605f961cab203b4d3476
kgu0515 <kgu0515@khu.ac.kr>
2021-10-08 11:24:15 +0900

    modefied

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void main() {
-	for (int i = 1; i < 9;i++) {
+	for (int i = 1; i <= 9;i++) { // = Ãß°¡
 		for (int j = 1; j <= 9; j++) {
 			cout << i << " x " << j << " = " << i * j << "\n";
 		}
